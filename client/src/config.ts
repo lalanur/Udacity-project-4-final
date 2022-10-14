acc47f8de236c827b970c0dcec4964d0dc8bd8b8
lalanur <lalanura5@gmail.com>
2022-10-14 20:08:18 +0000

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wpnyi78mzd';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-j6f4bif0.us.auth0.com',
+  clientId: 'HvjNEip1wwRxAQgSbOZ4NJkPX7qI5bQa',
+  callbackUrl: 'http://localhost:3000/callback'
+};
